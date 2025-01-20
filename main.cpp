14fe147257eb8ee34a5c936a4ae2282a1734b2a4
iansm <ians.mcvay@gmail.com>
2025-01-19 17:37:16 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    std::string firstName = "John";
+    std::string lastName = "Rambo";
+    std::string streetAddress = "123 Maple st.";
+    std::string city = "Wonderville";
+    int zipCode = 83432;
+
+    std::cout << "First Name: " << firstName << std::endl;
+    std::cout << "Last Name: " << lastName << std::endl;
+    std::cout << "Street Address: " << streetAddress << std::endl;
+    std::cout << "City: " << city << std::endl;
+    std::cout << "Zip Code: " << zipCode << std::endl;
+
+    return 0;
+}
